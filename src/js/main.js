45a10e93cd24651b261fe3e29ca5e4a5c13eabaf
DCI <daniel.mengesha@gmx.de>
2018-09-25 16:16:54 +0200

    add dependences

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,17 @@
 main project javascript file (entry point)
 */
 
+//import Javascript module
+
+import './_vendor'
+import './_app'
+
+//import css style
 import '../scss/main.scss';
-console.log(hi);
+
+
+
+
+
+
+
